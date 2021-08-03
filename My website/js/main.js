6cf01e2b207c920c93eec963ad1221bad83b65af
Ravivarma Balthu <68553151+Ravivarma-Balthu-05@users.noreply.github.com>
2021-08-03 10:21:40 +0530

    Add files via upload

--- My website/js/main.js
+++ My website/js/main.js
@@ -1,8 +1,16 @@
 'use strict';
 const nav = document.querySelector('.opn');
 const hide = document.querySelector('.hidden');
+const hamclse = document.querySelector('.close');
 
 nav.addEventListener('click',function(){
     hide.classList.remove('hidden');
-    console.log('clicked');
+    hamclse.classList.remove('hidden');
+    nav.classList.add('hidden');
 })
+hamclse.addEventListener('click',function(){
+    hide.classList.add('hidden');
+    hamclse.classList.add('hidden');
+    nav.classList.remove('hidden');
+})
+
