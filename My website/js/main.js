d4b122f67d2e7934540c271e0f498a8a7e1a2f7d
Ravivarma Balthu <68553151+Ravivarma-Balthu-05@users.noreply.github.com>
2021-08-12 07:45:20 +0530

    Dark Mode added for Desktop Version

--- My website/js/main.js
+++ My website/js/main.js
@@ -2,6 +2,10 @@
 const nav = document.querySelector('.opn');
 const hide = document.querySelector('.hidden');
 const hamclse = document.querySelector('.close');
+const white = document.querySelector('.WHITE');
+const dark = document.querySelector('.DARK');
+const mainDark = document.querySelector('.main');
+
 
 nav.addEventListener('click',function(){
     hide.classList.remove('hidden');
@@ -14,3 +18,15 @@ hamclse.addEventListener('click',function(){
     nav.classList.remove('hidden');
 })
 
+//Dark Mode
+
+white.addEventListener('click',function(){
+    dark.classList.remove('hidden');
+    white.classList.add('hidden');
+    mainDark.classList.add('dark_main');
+})
+dark.addEventListener('click',function(){
+    dark.classList.add('hidden');
+    white.classList.remove('hidden');
+    mainDark.classList.remove('dark_main');
+})
